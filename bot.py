b2de2fae9f321fb2cf5613daa292e0375a2a018d
Sergei <mikhailovse@mail.ru>
2019-03-16 14:50:04 +0300

    первое сообщение с помощью телебот

--- bot.py
+++ bot.py
@@ -1,48 +1,12 @@
-# -*- coding: utf-8 -*-
 import config
-import requests
 import telebot
-from time import sleep
 
 
-URL = "https://api.telegram.org/bot{}/".format(config.token)
-
-
-def get_updates_json():
-    response = requests.get(URL + 'getUpdates')
-    return response.json()
-
-
-def get_last_update(response):
-    result = response['result']
-    lens = len(result)
-    return result[lens-1]
-
-
-def send_echo_answer(lastupdate):
-    param = {'chat_id': lastupdate['message']['chat']['id'],
-             'text': lastupdate['message']['text']}
-    response = requests.post(URL + 'sendMessage', data=param)
-    return response
-
-
-def echo_bot_function():
-    lastupdateid = 0
-    while 1:
-        response = get_updates_json()
-        lastupdate = get_last_update(response)
-        if lastupdate['update_id'] == lastupdateid or \
-                lastupdate['message']['from']['is_bot'] == 'true':
-            pass
-        else:
-            lastupdateid = lastupdate['update_id']
-            send_echo_answer(lastupdate)
-        sleep(2)
+bot = telebot.TeleBot(config.token)
 
 
 def main():
-    echo_bot_function()
-
+    bot.send_message(config.my_chat_id, 'trying telebot lib')
 
 
 if __name__ == '__main__':
--- config.py
+++ config.py
@@ -1,4 +1,2 @@
-# -*- coding: utf-8 -*-
-
 token = '733240319:AAHZRY9w7JThKrkSZ_4cKKBQI_PH_QfHZ8A'
 my_chat_id = 350378109
