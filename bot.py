21e617a4f10b3f98421f6f0d5a26e0dbb34e215a
Sergei <mikhailovse@mail.ru>
2019-03-16 15:06:00 +0300

    распознователь типа сообщений

--- bot.py
+++ bot.py
@@ -7,11 +7,41 @@ bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=['text'])
 def common_answer(message):
-    print('пришло сообщение')
+    print('пришло обычное сообщение')
     bot.send_message(config.my_chat_id, 'я не понимаю, но ты все равно пиши чонить)')
 
 
+@bot.message_handler(content_types=['photo'])
+def common_answer(message):
+    print('пришла фотография')
+    bot.send_message(config.my_chat_id, 'ооо, а это фотка')
+
+@bot.message_handler(content_types=['sticker'])
+def common_answer(message):
+    print('пришел стикер')
+    bot.send_message(config.my_chat_id, 'это стикер')
+
+
+@bot.message_handler(content_types=['document'])
+def common_answer(message):
+    print('пришел документ')
+    bot.send_message(config.my_chat_id, 'это документик')
+
+
+@bot.message_handler(content_types=['audio'])
+def common_answer(message):
+    print('пришла аудиозапись')
+    bot.send_message(config.my_chat_id, 'аудиозапись')
+
+
+@bot.message_handler(content_types=['command'])
+def common_answer(message):
+    print('пришла команда')
+    bot.send_message(config.my_chat_id, 'а вот команды я пока не умею обрабатывать')
+
+
 def main():
+    bot.send_message(config.my_chat_id, 'дратути, я умею распознавать тип сообщений')
     bot.polling(none_stop=True, interval=0)
 
 
