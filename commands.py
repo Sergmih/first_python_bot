b32801a3e033e914c01c9a94ba9ff8f420c895a4
Sergei <mikhailovse@mail.ru>
2019-04-22 01:10:09 +0300

    поправлен график

--- commands.py
+++ commands.py
@@ -156,6 +156,7 @@ def create_plot_for_statistic(currency, from_date, to_date, bot, chat_id):
     labels = []
     for i in range(10):
         labels.append(i * n / 10)
+    labels.append(n)
     plt.xticks(labels, rotation=50)
     path = 'img/' + str(config.my_chat_id) + 'statistic_plot.png'
     plt.tight_layout()
